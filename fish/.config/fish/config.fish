a2bf288c45f5ea1b9bf6f9e6ff7f508c0570c1cd
Jesse Dailey <jesse.dailey@gmail.com>
2014-10-23 18:35:47 -0400

    update fish_prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,47 +8,67 @@ alias vpr="vagrant provision"
 alias vst="vagrant status"
 alias lsa="ls -hal"
 
-function fish_prompt --description "Write out a custom prompt"
-
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
-
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
-	end
-
-	set -l git_prompt (__fish_git_prompt)
-
-	if not set -q __fish_prompt_git
-		set -g __first_prompt_git (set_color green)
-	end
-
-	if not set -q __fish_prompt_cwd
-		set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-	end
-
+function _prompt_user
 	set_color yellow
 	printf '%s' $USER
 	set_color normal
+end
+
+function _prompt_host
+	if not set -q __fish_prompt_hostname
+		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+	end
 	printf ' at '
 	set_color magenta
 	printf $__fish_prompt_hostname
 	set_color normal
+end
+
+function _prompt_pwd
+	if not set -q __fish_prompt_cwd
+		set -g __fish_prompt_cwd (set_color green)
+	end
 	printf ' in '
 	set_color green
 	printf '%s' (prompt_pwd)
 	set_color normal
+end
+
+function _prompt_git
 	set -l git_prompt (__fish_git_prompt)
 	if test -n "$git_prompt"
 		printf ' on'
 		printf "$git_prompt"
 	end
 	set_color normal
+end
+
+function _prompt_screen
+	if not set -q __fish_prompt_screen
+		set -g __fish_prompt_screen (set_color cyan)
+	end
+	if test -n "$WINDOW"
+		printf $__fish_prompt_screen
+		printf ':%s' $WINDOW
+		set_color normal
+	end
+end
+
+function fish_prompt --description "Write out a custom prompt"
+
+	_prompt_user
+	_prompt_host
+	_prompt_screen
+	_prompt_pwd
+	_prompt_git
 	printf ' > '
 
 end
 
 if test -e /usr/libexec/java_home
 	set -g JAVA_HOME (/usr/libexec/java_home)
+end
+
+if test -e (which fish)
+	set -g SHELL (which fish)
 end
